9c6c00f832bace8b3b12dbd8d74aa250f77a376f
lmai5 <maihoanglai@gmail.com>
2023-03-05 15:09:47 +0900

    Update delete-multiple-function.sh

--- delete-multiple-function.sh
+++ delete-multiple-function.sh
@@ -2,5 +2,8 @@
 # Loop through the list of function names and delete each function
 for FUNCTION_NAME in "$@"
 do
-  gcloud functions delete "${FUNCTION_NAME}" --quiet
+  read temp <<< $(gcloud functions describe "${FUNCTION_NAME}" --region=asia-northeast1 | grep name)
+  IFS='/' read -r -a array <<< $temp
+  region=${array[3]}
+  gcloud functions delete "${FUNCTION_NAME}" --quiet --region="${region}"
 done
