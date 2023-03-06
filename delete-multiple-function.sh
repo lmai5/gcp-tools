546fe15c885da98853c1de0cf148dd446f2c145e
lmai5 <maihoanglai@gmail.com>
2023-03-06 18:54:17 +0900

    Update delete-multiple-function.sh

--- delete-multiple-function.sh
+++ delete-multiple-function.sh
@@ -2,15 +2,13 @@
 # Loop through the list of function names and delete each function
 for FUNCTION_NAME in "$@"
 do
-  # echo "gcloud functions list  --filter=\"NAME~${FUNCTION_NAME}\"  --format=\"value(REGION)\""
-  # region=$(gcloud functions list  --filter="NAME~$FUNCTION_NAME"  --format="value(REGION)")
-  region=$(gcloud functions list | grep -E '(^|\s)python-http-function($|\s)' | awk '{print $5}')
+  region=$(gcloud functions list | grep -E "(^|\s)${FUNCTION_NAME}($|\s)" | awk '{print $5}')
   # Check if region is empty
   if [[ -z $region ]]
   then
     echo "Failed to retrieve region for function ${FUNCTION_NAME}"
   else
-    echo "Deleting "
+    echo "Deleting function: ${FUNCTION_NAME} in region ${region}"
     gcloud functions delete "${FUNCTION_NAME}" --quiet --region="${region}"
   fi
 done
